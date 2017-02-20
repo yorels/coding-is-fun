a33259918cd3e5c18dcd502aa2a54c70f9e02539
yorels <yorels@icloud.com>
2017-02-19 22:26:25 -0600

    MVN-1 Adding sub-packages herarchy and config for Mvn project

---
+++ spring-web/src/main/java/mx/yorels/dev/config/AppConfig.java
@@ -0,0 +1,13 @@
+package mx.yorels.dev.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by yorels on 2017-02-19.
+ */
+@Configuration
+@ComponentScan(basePackages = "mx.yorels.dev")
+public class AppConfig {
+
+}
